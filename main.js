2b9939b04a3005a65bbabbf63f80889b884e3248
yokobot <11435354+yokobot@users.noreply.github.com>
2024-02-12 15:05:20 +0900

    Feature/1 init (#2)
    * init.
    
    * add node_modules.
    
    * delete node_modules from repo.
    
    * change to user vercel/ncc.
    
    * Only one file is supported from multiple files.
    
    * log output.
    
    * typo.
    
    * typo.
    
    * add trimWhitespace: false.
    
    * add qoute to inputString.
    
    * delete qoute to inputString.
    
    * fix option.
    
    * dist/index.js upload.

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const fs = require('fs')
+const core = require('@actions/core')
+const inputFile = core.getInput('file', { required: true })
+const inputLine = core.getInput('line', { required: true })
+const inputString = core.getInput('string', { required: true, trimWhitespace: false })
+
+const main = async () => {
+  // 指定されたファイルから特定行を読み込む
+  const data = fs.readFileSync(inputFile, 'utf8')
+  const line = data.split('\n')[inputLine - 1]
+  console.log(`Read line: ${line}`)
+  console.log(`Input string: ${inputString}`)
+
+  // 文字列を比較して、一致したらメッセージ、一致しなかったらエラーメッセージを出力
+  if (line === inputString) {
+    console.log('Matched')
+  } else {
+    throw new Error('Not matched')
+  }
+}
+
+main()
