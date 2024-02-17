a9759310c59335c4d57456e8487f74f99aa16215
yokobot <takuou@gmail.com>
2024-02-17 11:52:18 +0900

    bug fix.

--- main.js
+++ main.js
@@ -7,21 +7,17 @@ const inputLine = core.getInput('line', { required: true })
 const inputString = core.getInput('string', { required: true, trimWhitespace: false })
 
 const main = async () => {
-  try {
-    // Reads a specific line from a specified file.
-    const data = fs.readFileSync(inputFile, 'utf8')
-    const line = data.split('\n')[inputLine - 1]
-    core.info(`Read line: ${line}`)
-    core.info(`Input string: ${inputString}`)
+  // Reads a specific line from a specified file.
+  const data = fs.readFileSync(inputFile, 'utf8')
+  const line = data.split('\n')[inputLine - 1]
+  core.info(`Read line: ${line}`)
+  core.info(`Input string: ${inputString}`)
 
-    // Compares a input string with a specific line.
-    if (line === inputString) {
-      core.info('String is matched')
-    } else {
-      core.error('String is not matched!!!')
-    }
-  } catch (error) {
-    core.setFailed(error.message)
+  // Compares a input string with a specific line.
+  if (line === inputString) {
+    core.info('String is matched')
+  } else {
+    core.error('String is not matched!!!')
   }
 }
 
